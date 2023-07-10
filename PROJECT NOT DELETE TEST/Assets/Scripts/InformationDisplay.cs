73a6d280718249ae72924657312859a54664a9df
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2023-07-10 13:57:59 +0100

    Offset script modification / CanvasRotationPoint

--- PROJECT NOT DELETE TEST/Assets/Scripts/InformationDisplay.cs
+++ PROJECT NOT DELETE TEST/Assets/Scripts/InformationDisplay.cs
@@ -7,27 +7,38 @@ public class InformationDisplay : MonoBehaviour
 {
     [Header("Information Display Config:")]
     [SerializeField] TextMeshProUGUI infomation;
-    [SerializeField] Transform canvasRotationPoint; 
+    [SerializeField] Transform canvasRotationPointPrefab;
     [SerializeField] Transform playerCamera;
-    [SerializeField] float distance = 5f;
+    [SerializeField] float distance = 1;
+    [SerializeField] float offset = 5;
 
-    private Transform rotationPivot;
+    private Transform canvasRotationPoint;
+    private Transform canvasRotationPoint2;
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationPivot = GetComponent<Transform>();
-
         playerCamera = FindObjectOfType<Camera>().gameObject.transform;
+        canvasRotationPoint = Instantiate(canvasRotationPointPrefab, transform);
+
+                    // pos calc
+        Vector3 offsetPosition = -canvasRotationPoint.right * offset;
+        canvasRotationPoint2 = Instantiate(canvasRotationPointPrefab, transform.position + offsetPosition, transform.rotation);
     }
 
-    public void DisplayInformation(string objectInformation) {
+    public void DisplayInformation(string objectInformation)
+    {
         infomation.text = objectInformation;
 
         canvasRotationPoint.position = playerCamera.transform.position + playerCamera.transform.forward * distance;
         canvasRotationPoint.position = new Vector3(canvasRotationPoint.position.x, 1f, canvasRotationPoint.position.z);
         canvasRotationPoint.rotation = new Quaternion(0f, playerCamera.transform.rotation.y, 0f, playerCamera.transform.rotation.w);
         canvasRotationPoint.gameObject.SetActive(true);
+
+        
+        Vector3 offsetPosition = -canvasRotationPoint.right * offset;
+        canvasRotationPoint2.position = canvasRotationPoint.position + offsetPosition;
+        canvasRotationPoint2.rotation = canvasRotationPoint.rotation;
+        canvasRotationPoint2.gameObject.SetActive(true);
     }
-    
 }
