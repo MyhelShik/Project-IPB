256caad84e2785d3db8d82b5d2429cb266ae0b71
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2023-07-11 23:08:50 +0100

    11/07 - An implementation of UnityWebRequest
    InfoDisplay and mapping modification while the method used in InfoD"" is resolved.

--- PROJECT NOT DELETE TEST/Assets/Scripts/Serializable Classes/Mapping.cs
+++ PROJECT NOT DELETE TEST/Assets/Scripts/Serializable Classes/Mapping.cs
@@ -1,9 +1,12 @@
 [System.Serializable]
 public class Mapping 
 {
-    
+
     public string subject; 
     public string content;
+    
+
+
     public string uuidsensor;
     public string type;
     public string information;
