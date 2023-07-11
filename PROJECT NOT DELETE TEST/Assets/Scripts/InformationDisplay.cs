7b2ec3661d770f8fef8817fc2f389af1fcf8bb2a
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2023-07-11 23:21:54 +0100

    Update InformationDisplay.cs
    da

--- PROJECT NOT DELETE TEST/Assets/Scripts/InformationDisplay.cs
+++ PROJECT NOT DELETE TEST/Assets/Scripts/InformationDisplay.cs
@@ -4,7 +4,23 @@
     using TMPro;
     using UnityEngine.Networking;
 
+<<<<<<< HEAD
     public class InformationDisplay : MonoBehaviour
+=======
+public class InformationDisplay : MonoBehaviour
+{
+    [Header("Information Display Config:")]
+    [SerializeField] TextMeshProUGUI infomation;
+    [SerializeField] Transform canvasRotationPointPrefab;
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float distance = 1;
+    [SerializeField] float offset = 5;
+
+    private Transform canvasRotationPoint;
+
+    // Start is called before the first frame update
+    void Start()
+>>>>>>> 309eacf90135a1fa90ba7641bee5027a622c1120
     {
         [Header("Information Display Config:")]
         [SerializeField] TextMeshProUGUI infomation;
@@ -13,6 +29,7 @@
         [SerializeField] float distance = 1;
         [SerializeField] float offset = 5;
 
+<<<<<<< HEAD
         private Transform canvasRotationPoint;
         private Transform canvasRotationPoint2;
 
@@ -67,3 +84,23 @@
             canvasRotationPoint.gameObject.SetActive(true);
         }
     }
+=======
+        // Calculate the position for the second canvasRotationPoint? No need ig
+        Vector3 offsetPosition = -canvasRotationPoint.right * offset;
+    }
+
+    public void DisplayInformation(string sensorUuid)
+    {
+        Debug.Log(sensorUuid);
+        infomation.text = sensorUuid;
+
+
+
+        canvasRotationPoint.position = playerCamera.transform.position + playerCamera.transform.forward * distance;
+        canvasRotationPoint.position = new Vector3(canvasRotationPoint.position.x, 1f, canvasRotationPoint.position.z);
+        canvasRotationPoint.rotation = new Quaternion(0f, playerCamera.transform.rotation.y, 0f, playerCamera.transform.rotation.w);
+        canvasRotationPoint.gameObject.SetActive(true);
+
+    }
+}
+>>>>>>> 309eacf90135a1fa90ba7641bee5027a622c1120
